0fdae6a31f7d598e969f2209a122dfb94019d8f1
43735 <4373531312@qq.com>
2020-02-20 00:06:12 +0800

    del trush

--- src/main/java/top/kxyu/springbootjava/SpringBootJavaApplication.java
+++ src/main/java/top/kxyu/springbootjava/SpringBootJavaApplication.java
@@ -1,13 +1,15 @@
 package top.kxyu.springbootjava;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import top.kxyu.springbootjava.service.VideoService;
 
+//方法二implements CommandLineRunner
 @SpringBootApplication
-public class SpringBootJavaApplication {
+public class SpringBootJavaApplication  {
 
 
 	public static void main(String[] args) {
@@ -18,4 +20,16 @@ public class SpringBootJavaApplication {
 
 	}
 
+	/*//方法二实现CommandLineRunner
+	@Autowired
+	private VideoService videoService;
+	public static void main(String[] args) {
+	//启动spring容器
+		SpringApplication.run(SpringBootJavaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(videoService.Say("ABCD"));
+	}*/
 }
